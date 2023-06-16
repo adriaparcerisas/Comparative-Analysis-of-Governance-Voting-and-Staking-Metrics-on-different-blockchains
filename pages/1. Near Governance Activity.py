38bbbeb58766376e9b53b458a9844c162218801c
Adrià Parcerisas <adria.parcerisas.albes@gmail.com>
2023-06-16 12:08:27 +0200

    Update 1. Near Governance Activity.py

--- pages/1. Near Governance Activity.py
+++ pages/1. Near Governance Activity.py
@@ -73,7 +73,7 @@ order by 1 asc
 )
 select 
 trunc(date,'month') as date,
-avg(balance) as total_near_delegated
+avg(total_near_delegated) as total_near_delegated
 from final --where date>=current_date - INTERVAL '3 MONTHS'
 group by 1
 """
@@ -98,7 +98,7 @@ else 'Others' end as ranks,
 FROM top10
 group by 1,2
 order by 1 asc 
- ) select trunc(date,'month') as date, ranks,avg(balance) as total_near_delegated from final where date>=current_date - INTERVAL '3 MONTHS'
+ ) select trunc(date,'month') as date, ranks,avg(total_near_delegated) as total_near_delegated from final where date>=current_date - INTERVAL '3 MONTHS'
 
 
 """
